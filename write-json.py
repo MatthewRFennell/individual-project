3138af0549d2f113e2b08fbb0c9c8ad3163b3357
matthew <matthew.robert.fennell@gmail.com>
2021-05-13 21:21:38 +0100

    Update write-json.py to consistently handle pthread_create breakpoints in local scope

--- write-json.py
+++ write-json.py
@@ -11,20 +11,25 @@ class BreakListener:
 		self.hit_breakpoints = thread_hit_list
 
 	def __call__(self, event):
-		if event.breakpoints[0].type == 9:
-			frame = gdb.selected_frame()
-		else:
+		frame = gdb.selected_frame()
+		if event.breakpoint in thread_related_breakpoints:
 			frame = gdb.selected_frame().older()
-		self.hit_breakpoints.append({"thread": gdb.selected_thread().num, "hits": hex(frame.pc())})
+		thread_event = ""
+		if event.breakpoint == pthread_create:
+			thread_event = "pthread_create"
+		elif event.breakpoint == pthread_join:
+			thread_event = "pthread_join"
+		self.hit_breakpoints.append({"thread": gdb.selected_thread().num, "hits": hex(frame.pc()), "thread_event": thread_event})
 		gdb.execute("continue")
 
 hit_breakpoints = {}
 hit_breakpoints["checkpoints"] = []
+thread_related_breakpoints = []
 
 counter = gdb.Breakpoint("counter", gdb.BP_WATCHPOINT, gdb.WP_ACCESS)
 pthread_create = gdb.Breakpoint("pthread_create", gdb.BP_BREAKPOINT)
 pthread_join = gdb.Breakpoint("pthread_join", gdb.BP_BREAKPOINT)
-increment = gdb.Breakpoint("increment", gdb.BP_BREAKPOINT)
+thread_related_breakpoints.extend([pthread_create, pthread_join])
 listener = BreakListener(hit_breakpoints["checkpoints"])
 gdb.events.stop.connect(listener)
 gdb.execute("run")
