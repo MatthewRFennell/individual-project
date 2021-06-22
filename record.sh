4d89ce88287c5d9d206e7981824ee819a52edd59
Matthew Fennell <mf2017@ic.ac.uk>
2021-06-22 23:41:41 +0100

    Redirect gdb and syrup output to keep stdout clean

--- record.sh
+++ record.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-../gdb-build/bin/gdb ./examples/counter-2-threads/a.out -x syrup/write-json.py #> record.log 2>&1
-../gdb-build/bin/gdb ./examples/counter-2-threads/a.out -x syrup/read-json.py #> replay.log 2>&1 && cat execution.log
+../gdb-build/bin/gdb ./examples/counter-2-threads/a.out -x syrup/write-json.py > record.log 2>&1
+../gdb-build/bin/gdb ./examples/counter-2-threads/a.out -x syrup/read-json.py > replay.log 2>&1 && cat execution.log
--- replay.sh
+++ replay.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../gdb-build/bin/gdb ./examples/counter-2-threads/a.out -x syrup/read-json.py #> replay.log 2>&1 && cat execution.log
+../gdb-build/bin/gdb ./examples/counter-2-threads/a.out -x syrup/read-json.py > replay.log 2>&1 && cat execution.log
